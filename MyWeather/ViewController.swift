9158f4d557a9f5b4f7fd960e111ed585a6b1cfe5
SmartAOFsDev <delelop.mistine@gmail.com>
2020-07-08 10:25:48 +0700

    Initial Commit

---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by SD-M004 on 8/7/2563 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
